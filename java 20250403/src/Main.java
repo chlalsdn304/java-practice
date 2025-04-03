06995baaca78174b403d403b8a1e127c5b61a7de
minwoo <choiminwoo5968@gmail.com>
2025-04-03 16:33:43 +0900

    학점출력

---
+++ java 20250403/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        double kor,eng,math,sci,sum,doubleAvg;
+        int intAvg;
+        boolean boolResult=false;
+        System.out.print("국어점수를 입력하세요");
+        kor = sc.nextDouble();
+        System.out.print("영어점수를 입력하세요");
+        eng = sc.nextDouble();
+        System.out.print("수학점수를 입력하세요");
+        math = sc.nextDouble();
+        System.out.print("과학점수를 입력하세요");
+        sci = sc.nextDouble();
+
+        sum = kor+eng+math+sci;
+        intAvg = (int)(sum/4);
+        doubleAvg = sum/4;
+        //boolResult=intAvg==doubleAvg;
+
+        System.out.printf("총점: %.2f\n",sum);
+        System.out.printf("int 평균값: %d\n",intAvg);
+        System.out.printf("double 평균값: %.2f\n",doubleAvg);
+
+
+
+
+    }
+}
