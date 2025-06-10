85a0e89c1c9666832977db6a65f5ac9b5941ab72
minwoo <choiminwoo5968@gmail.com>
2025-06-10 22:20:26 +0900

    ReservationManager

---
+++ java project/src/ReservationManager.java
@@ -0,0 +1,7 @@
+public class ReservationManager {
+    private SeatManager seatManager;
+
+    public ReservationManager(SeatManager seatManager) {
+        this.seatManager = seatManager;
+    }
+}
