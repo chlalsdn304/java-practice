226e3b64ca521d7aff4a5416519e13f958efc32c
minwoo <choiminwoo5968@gmail.com>
2025-06-13 21:07:34 +0900

    0612

--- java project/src/ReservationManager.java
+++ java project/src/ReservationManager.java
@@ -4,4 +4,15 @@ public class ReservationManager {
     public ReservationManager(SeatManager seatManager) {
         this.seatManager = seatManager;
     }
+
+    public void reserveSeat(int num, String name) {
+        if (num < 1 || num > 10) {
+            System.out.println("잘못된 좌석 번호입니다.");
+        } else if (seatManager.getSeat(num - 1) != null) {
+            System.out.println("이미 예약된 좌석입니다.");
+        } else {
+            seatManager.setSeat(num - 1, name);
+            System.out.println("예약이 완료되었습니다.");
+        }
+    }
 }
